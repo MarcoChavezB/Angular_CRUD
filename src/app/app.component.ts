f3b4f3bff7d68c7014fc5e71e23230abee5e24b3
Marco <marco1102004@gmail.com>
2024-03-01 21:33:10 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+/*
+  Compo
+*/
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ButtonComponent,
+    FooterComponent,
+    HeaderComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './Layouts/auth/auth.component';
+import { LoginComponent } from './Views/Auth/login/login.component';
+import { RegisterComponent } from './Views/Auth/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ 
+  {
+    /*
+    Cambiar esto si no tiene el token 
+    */
+    path: '',
+    component: AuthComponent,  
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
